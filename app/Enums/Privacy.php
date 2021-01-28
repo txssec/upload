c5feba132b8cb97cd959936a2f99716632465e18
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-01-28 16:08:45 -0300

    feat(enum): create types privacy enum

---
+++ app/Enums/Privacy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method privacy enum
+ */
+final class Privacy extends Enum
+{
+    const PRIVATE =  'private';
+    const PUBLIC =   'public';
+}
